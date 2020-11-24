4453454c5de4a9b7ce08163f0b53bf9bbc879e6c
Vinícius Rodrigues Godoy dos Santos <v.godoyrodrigues@gmail.com>
2020-11-23 23:53:43 -0300

    Adiciona Funções de E/S e um Menu
    Adiciona as funções de escrita e leitura do arquivo, menu de cadastro, zerar estrutura, além de arrumar referência do ponteiro 'ps' e arrumar estrutura switch-case

--- C05.c
+++ C05.c
@@ -3,11 +3,14 @@
 #include <conio.h> //Funções getch e getche
 #include <locale.h> //Função de configuração de idioma da tabela ASCII
 
+//Macros
+#define NUM_PROD 5 //Macro com o número de produtos, basta aumentar aqui para mudar o programa.
+
 //Estrutura
 struct estoque{
     unsigned char nome[31];
     int qtd_atual; //quantidade atual
-    int qtd_min;   //quantidaded de estoque mínima
+    int qtd_min;   //quantidade de estoque mínima
     int dia_validade;
     int mes_validade;
     int ano_validade;
@@ -15,16 +18,19 @@ struct estoque{
 
 //Protótipo das Funções
 void lbuffer(void); //Função para limpar o buffer que funciona em todos os SOs
-void menu_cadastrar();
-void menu_listar();
-void menu_pesquisar_nome();
-void menu_pesquisar_letra();
-void menu_pesquisar_validade();
-void menu_listar_qtdmin();
-void menu_movimentar();
-void menu_alterar();
-void menu_excluir();
-void sair();
+void escreverEstrutura(struct estoque *ps, char modo[], int linha); //Função pra escrever uma estrutura no arquivo
+void lerEstrutura(struct estoque *ps, int linha); //Função pra ler uma estrutura no arquivo
+void zerar_estrutura(struct estoque *ps); //Função que zera todas as variáveis da estrutura
+void menu_cadastrar(struct estoque *ps); //Função do menu de cadastramento de produtos
+void menu_listar(struct estoque *ps);
+void menu_pesquisar_nome(struct estoque *ps);
+void menu_pesquisar_letra(struct estoque *ps);
+void menu_pesquisar_validade(struct estoque *ps);
+void menu_listar_qtdmin(struct estoque *ps);
+void menu_movimentar(struct estoque *ps);
+void menu_alterar(struct estoque *ps);
+void menu_excluir(struct estoque *ps);
+void sair(struct estoque *ps);
 
 //Programa
 void main(void)
@@ -33,6 +39,8 @@ void main(void)
     setlocale(LC_ALL, "portuguese"); //Configura o idioma padrão da ASCII pra português
     static struct estoque produto, *ps; //Declaração da estrutura e seu ponteiro
     int menu; //Declaração da variável do menu principal
+
+    ps = &produto;
 
     printf("MENU\n");
     printf(" 1. Cadastrar produto\n");
@@ -48,40 +56,56 @@ void main(void)
     printf("\nResposta -> ");
 
     do{scanf("%d", &menu);}while(menu != 1 && menu != 2 && menu != 3 && menu != 4 && menu != 5 && menu != 6 && menu != 7 && menu != 8 && menu != 9 && menu != 10);
-    system("cls"); //Sempre que sair do menu limpar a tela
-    lbuffer(); //Sempre que sair do menu limpar o buffer
 
     switch(menu)
     {
     case 1:
-        menu_cadastrar();
+        system("cls"); //Sempre que sair do menu limpar a tela
+        lbuffer(); //Sempre que sair do menu limpar o buffer
+        menu_cadastrar(ps);
         break;
     case 2:
-        menu_listar();
+        system("cls"); //Sempre que sair do menu limpar a tela
+        lbuffer(); //Sempre que sair do menu limpar o buffer
+        menu_listar(ps);
         break;
     case 3:
-        menu_pesquisar_nome();
+        system("cls"); //Sempre que sair do menu limpar a tela
+        lbuffer(); //Sempre que sair do menu limpar o buffer
+        menu_pesquisar_nome(ps);
         break;
     case 4:
-        menu_pesquisar_letra();
+        system("cls"); //Sempre que sair do menu limpar a tela
+        lbuffer(); //Sempre que sair do menu limpar o buffer
+        menu_pesquisar_letra(ps);
         break;
     case 5:
-        menu_pesquisar_validade();
+        system("cls"); //Sempre que sair do menu limpar a tela
+        lbuffer(); //Sempre que sair do menu limpar o buffer
+        menu_pesquisar_validade(ps);
         break;
     case 6:
-        menu_listar_qtdmin();
+        system("cls"); //Sempre que sair do menu limpar a tela
+        lbuffer(); //Sempre que sair do menu limpar o buffer
+        menu_listar_qtdmin(ps);
         break;
     case 7:
-        menu_movimentar();
+        system("cls"); //Sempre que sair do menu limpar a tela
+        lbuffer(); //Sempre que sair do menu limpar o buffer
+        menu_movimentar(ps);
         break;
     case 8:
-        menu_alterar();
+        system("cls"); //Sempre que sair do menu limpar a tela
+        lbuffer(); //Sempre que sair do menu limpar o buffer
+        menu_alterar(ps);
         break;
     case 9:
-        menu_excluir();
+        system("cls"); //Sempre que sair do menu limpar a tela
+        lbuffer(); //Sempre que sair do menu limpar o buffer
+        menu_excluir(ps);
         break;
     case 10:
-        sair();
+        sair(ps);
         break;
     }
 
@@ -96,52 +120,132 @@ void lbuffer(void)
     } while (c != '\n' && c != EOF);
 }
 
-void menu_cadastrar()
+void escreverEstrutura(struct estoque *ps, char modo[], int linha)
 {
-    printf("1"); //Teste do menu
+    FILE *file;
+    int i;
+    linha = 0;//linha * 125;
+
+    file = fopen("estoque.txt", modo);
+    if(linha != 0){fseek(file, linha, SEEK_SET);}
+    fwrite(ps -> nome, sizeof(ps -> nome), 1, file);
+    fwrite(&ps -> qtd_atual, sizeof(ps -> qtd_atual), 1, file);
+    fwrite(&ps -> qtd_min, sizeof(ps -> qtd_min), 1, file);
+    fwrite(&ps -> dia_validade, sizeof(ps -> dia_validade), 1, file);
+    fwrite(&ps -> mes_validade, sizeof(ps -> mes_validade), 1, file);
+    fwrite(&ps -> ano_validade, sizeof(ps -> ano_validade), 1, file);
+    fclose(file);
+
+    zerar_estrutura(ps);
 }
 
-void menu_listar()
+void lerEstrutura(struct estoque *ps, int linha)
 {
-    printf("2"); //Teste do menu
+    FILE *file;
+    int i;
+    linha = 0;//linha * 125;
+
+    zerar_estrutura(ps);
+
+    file = fopen("estoque.txt", "r");
+    fseek(file, linha, SEEK_SET);
+    fread(ps -> nome, sizeof(ps -> nome), 1, file);
+    fread(&ps -> qtd_atual, sizeof(ps -> qtd_atual), 1, file);
+    fread(&ps -> qtd_min, sizeof(ps -> qtd_min), 1, file);
+    fread(&ps -> dia_validade, sizeof(ps -> dia_validade), 1, file);
+    fread(&ps -> mes_validade, sizeof(ps -> mes_validade), 1, file);
+    fread(&ps -> ano_validade, sizeof(ps -> ano_validade), 1, file);
+    fclose(file);
 }
 
-void menu_pesquisar_nome()
+void zerar_estrutura(struct estoque *ps)
+{
+    int i;
+
+    for(i = 0; i < (sizeof(ps -> nome) - 1); i++){
+        ps -> nome[i] = '\0';
+    }
+    ps -> qtd_atual = 0;
+    ps -> qtd_min = 0;
+    ps -> dia_validade = 0;
+    ps -> mes_validade = 0;
+    ps -> ano_validade = 0;
+}
+
+void menu_cadastrar(struct estoque *ps)
+{
+    int i;
+    char menu;
+
+    for(i = 0; i < NUM_PROD; i++){
+        printf("Entre com os dados do produto que deseja cadastrar!\n\n");
+        printf("Nome do produto %d: ", i + 1);
+        gets(ps -> nome);
+        printf("Quantidade Atual do produto %d: ", i + 1);
+        scanf("%d", &ps -> qtd_atual);
+        printf("Quantidade Mínima do produto %d: ", i + 1);
+        scanf("%d", &ps -> qtd_min);
+        printf("Data de validade do produto %d(DD/MM/AA): ", i + 1);
+        scanf("%d/%d/%d", &ps -> dia_validade, &ps -> mes_validade, &ps -> ano_validade);
+
+        if(i == 0){
+            escreverEstrutura(ps, "w", 0);
+        }else{
+            escreverEstrutura(ps, "a", 0);
+        }
+        lbuffer();
+        system("cls");
+    }
+    system("cls");
+    printf("\aTODOS OS DADOS DE PRODUTO FORAM INSERIDOS!\n");
+    printf("\nPressione 'B' para voltar ao menu\n");
+    do{menu = getch();}while(menu != 'B' && menu != 'b');
+    if(menu == 'B' || menu == 'b'){system("cls");main();}
+}
+
+void menu_listar(struct estoque *ps)
+{
+    printf("Quantidade Atual do produto 1: ");
+    scanf("%d", &ps->qtd_atual);
+    printf("%d", ps->qtd_atual);
+}
+
+void menu_pesquisar_nome(struct estoque *ps)
 {
     printf("3"); //Teste do menu
 }
 
-void menu_pesquisar_letra()
+void menu_pesquisar_letra(struct estoque *ps)
 {
     printf("4"); //Teste do menu
 }
 
-void menu_pesquisar_validade()
+void menu_pesquisar_validade(struct estoque *ps)
 {
     printf("5"); //Teste do menu
 }
 
-void menu_listar_qtdmin()
+void menu_listar_qtdmin(struct estoque *ps)
 {
     printf("6"); //Teste do menu
 }
 
-void menu_movimentar()
+void menu_movimentar(struct estoque *ps)
 {
     printf("7"); //Teste do menu
 }
 
-void menu_alterar()
+void menu_alterar(struct estoque *ps)
 {
     printf("8"); //Teste do menu
 }
 
-void menu_excluir()
+void menu_excluir(struct estoque *ps)
 {
     printf("9"); //Teste do menu
 }
 
-void sair()
+void sair(struct estoque *ps)
 {
     printf("10"); //Teste do menu
 }
